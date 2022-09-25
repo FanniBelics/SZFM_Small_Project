1ecedf63cb2f80734a7f39befb577c64aa570905
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2022-09-26 01:52:18 +0200

    admin felulet torles gomb, vissza gomb, regisztracio ellenorzes, pontok visszamentese adatbazisba

--- code/app.py
+++ code/app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+import email
 import sqlite3 as sql
 import hashlib as hl
 from flask import Flask, g, render_template, request, session
@@ -12,7 +14,6 @@ def index():
     session["levels"] = get_levels()
     session["roles"] = get_roles()
     session["userlevels"] = get_userlevel()
-    #print(session["users"],session["images"],session["levels"],session["roles"],session["userlevels"])
     return render_template('layout.html')
 
 @app.route('/add_user', methods=['POST'])
@@ -24,24 +25,27 @@ def new_user():
         cursor = db.cursor()
 
         e_mail = request.form['e_mail']
-        print('USER EMAIL: ',e_mail)
+        if(len(e_mail) == 0):
+            print('Please enter email')
+            return render_template('layout.html')
         user_name = request.form['user_name']
-        print('USER NAME: ',user_name)
+        if(len(user_name) < 3):
+            print('User name must contain at least 3 characters')
+            return render_template('layout.html')
         user_psw = request.form['user_psw']
+        if(len(user_name) == 0):
+            print('Please enter password')
+            return render_template('layout.html')
         encrypted = hl.md5(user_psw.encode()).hexdigest()
-        print('USER PSW: ',user_psw, ' ENCODED: ',encrypted)
 
         cursor.execute("INSERT INTO user (email, user_name, user_psw, total_score, role_id) VALUES (?,?,?,?,?)",
         (e_mail, user_name, encrypted,0,2))
 
         db.commit()
 
-        msg = 'Record successfully added'
     except:
         db.rollback()
-        msg = 'Error'
     finally:
-        print(msg)
         db.close()
 
     return render_template('layout.html')
@@ -53,22 +57,19 @@ def login():
         db = g._database = sql.connect('user_database.db')
     cursor = db.cursor()
 
-    email = request.form['login_email']
-    psw = request.form['login_psw']
-    encrypted = hl.md5(psw.encode()).hexdigest()
 
-    print('EMAIL: ',email,' PSW: ',psw, ' ENCRYPTED: ',encrypted)
+    session["email"] = request.form['login_email']
+    session["psw"] = request.form['login_psw']
+    encrypted = hl.md5(session["psw"].encode()).hexdigest()
 
-    cursor.execute("SELECT user_name FROM user WHERE email = ? AND user_psw = ?",(email,encrypted))
+    cursor.execute("SELECT user_name FROM user WHERE email = ? AND user_psw = ?",(session["email"],encrypted))
     record = cursor.fetchone()
     if record is not None:
         session["user_name"] = record[0]
-        print(session["user_name"])
         cursor.execute("SELECT total_score, role_id FROM user WHERE user_name = ?",(session["user_name"],))
         user = cursor.fetchone()
         session["total_score"] = user[0]
         session["role_id"] = user[1]
-        print('USER NAME: ',session["user_name"], 'SCORE: ',session["total_score"], 'ROLE: ',session["role_id"])
         return render_template('game.html', user_name=session["user_name"], total_score=session["total_score"], role_id=session["role_id"])
     return render_template('layout.html')
 
@@ -76,6 +77,8 @@ def login():
 def logout():
     session["user_name"] = None
     session["total_score"] = None
+    session["email"] = None
+    session["psw"] = None
 
     return render_template('layout.html')
 
@@ -85,11 +88,49 @@ def admin():
 
     return render_template('admin.html',users=session["users"])
 
+@app.route('/back_from_admin', methods=['POST'])
+def go_back(): 
+    return render_template('game.html', user_name=session["user_name"], total_score=session["total_score"], role_id=session["role_id"])
+
 @app.route('/claimpoints', methods=['POST'])
 def claim():
-    session["total_score"] += 10
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sql.connect('user_database.db')
+    cursor = db.cursor()
+    try:
+        cursor.execute('UPDATE user SET total_score = (total_score + 10) WHERE email = ?',(session['email'],))
+        session["total_score"] += 10
+        db.commit()
+    except:
+        db.rollback()
 
     return render_template('game.html', user_name=session["user_name"], total_score=session["total_score"], role_id=session["role_id"])
+
+@app.route('/delete_users', methods=['POST'])
+def remove_users():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sql.connect('user_database.db')
+    cursor = db.cursor()
+
+    checked_users = request.form.getlist("check")
+
+    for email in checked_users:
+        for user in session["users"]:
+            if user[0] == email:
+                try:
+                    cursor.execute('DELETE FROM user WHERE email = ?',(email,))
+                    db.commit()
+
+                    index = session["users"].index(user)
+                    session["users"].pop(index)
+
+                except:
+                    db.rollback()
+
+
+    return admin()
 
 def get_users():
     db = getattr(g, '_database', None)
